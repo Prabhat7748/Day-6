808d978855c1d30293cd3c75b479487e30cab631
Prabhat Kumar <109503226+Prabhat7748@users.noreply.github.com>
2022-07-21 10:25:01 +0530

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,28 @@
+# multiple inheritance
+ 
+# Base class1
+class Mother:
+    mothername = ""
+ 
+    def mother(self):
+        print(self.mothername)
+ 
+# Base class2
+ 
+ 
+class Father:
+    fathername = ""
+ 
+    def father(self):
+        print(self.fathername)
+ 
+ 
+class Son(Mother, Father):
+    def parents(self):
+        print("Father :", self.fathername)
+        print("Mother :", self.mothername)
+ 
+s1 = Son()
+s1.fathername = "RAM"
+s1.mothername = "SITA"
+s1.parents()
---
+++ polymorphism.py
@@ -0,0 +1,7 @@
+# Polymorphism
+ 
+def add(x, y, z = 0):
+    return x + y + z
+
+print(add(2, 3))
+print(add(2, 3, 4))
